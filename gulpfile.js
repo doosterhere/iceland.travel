8b0c7cc4079ee251aff05cceaaf7fddc7fc166b7
Mikalai Shcharbakou <doosterhere@gmail.com>
2023-04-26 09:16:09 +0300

    build has been completed

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,18 @@ exports.default = function () {
     return gulp.src('./src/less/style.less')
         .pipe(less().on('error', console.error.bind(console)))
         .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist'));
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./output'));
 }
 
 exports.watch = function () {
     gulp.watch('./src/less/*.less', gulp.series('default'));
+}
+
+exports.build = function () {
+    return gulp.src(['node_modules/jquery/dist/jquery.min.js',
+        'node_modules/wowjs/dist/wow.min.js',
+        'node_modules/slick-carousel/slick/slick.min.js',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.min.js'])
+        .pipe(gulp.dest('./output'));
 }
